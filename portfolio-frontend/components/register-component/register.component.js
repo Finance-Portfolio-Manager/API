cc609ce11eb810edabfdc5669357a421bf013d94
@aaronparker2319 <aaronparker2319@gmail.com>
2021-07-28 18:53:26 -0400

    validation is proving difficult

--- portfolio-frontend/components/register-component/register.component.js
+++ portfolio-frontend/components/register-component/register.component.js
@@ -1,13 +1,60 @@
-document.getElementById("registration-form").addEventListener("submit", function(register){
+const form = document.getElementById("registration-form");
 
-    const firstName = document.getElementById("first-name").value;
-    const lastName = document.getElementById("last-name").value;
-    const email = document.getElementById("email").value;
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-    const secondPassword = document.getElementById("repeat-password").value;
+const firstName = document.getElementById("first-name").value;
+const lastName = document.getElementById("last-name").value;
+const email = document.getElementById("email").value;
+const username = document.getElementById("username").value;
+const password = document.getElementById("password").value;
+const secondPassword = document.getElementById("repeat-password").value;
 
+function validateInput(firstName, lastName, email, username, password, secondPassword){
+    const nError = document.getElementById("name-error");
+    const eError = document.getElementById("email-error");
+    const unError = document.getElementById("username-error");
+    const pError = document.getElementById("password-error");
+    const rpError = document.getElementById("retype-password-error");
+    const letters = /^[A-Za-z]+$/;
+    
+    if(!firstName.match(letters) || !lastName.match(letters)){
+        nError.innerHTML = "<small style=\"color:Red\">First and last name may only contain letters<small>";
+        return false;
+    }
+    
+    if(lastName==="" || lastName==null){
+        // messages.push("Last name is required")
+    }
+    
+    if(email==="" || email==null){
+        // messages.push("Email is required")
+    }
+    
+    if(username==="" || username==null){
+        // messages.push("Username is required")
+    }
+    
+    if(password==="" || password==null){
+        // messages.push("Password is required")
+    }
+}
+
+form.addEventListener("submit", submitListener);
+
+function submitListener(register){
     register.preventDefault(); 
+
+    let isValidInput = validateInput(firstName, lastName, email, username, password, secondPassword);
+
+    // while(!isValidInput){
+    //     isValidInput = validateInput(firstName, lastName, email, username, password, secondPassword);
+    // }
+    if (isValidInput){
+        submit();
+    } else {
+        form.addEventListener("submit", submitListener);
+    }
+}
+
+function submit(){
     const credentials = {
         firstName:document.getElementById("first-name").value,
         lastName:document.getElementById("last-name").value,
@@ -24,4 +71,4 @@ document.getElementById("registration-form").addEventListener("submit", function
         }).then(response => response.json())
             .then(data => console.log(data))
             .catch(error => console.error(error)); 
-});
+}
