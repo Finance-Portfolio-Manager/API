16993f9315e2e6580dca51bf5b18074cab9aab8d
@aaronparker2319 <aaronparker2319@gmail.com>
2021-07-29 01:08:35 -0400

    validation almost working!

--- portfolio-frontend/components/register-component/register.component.js
+++ portfolio-frontend/components/register-component/register.component.js
@@ -1,41 +1,4 @@
-const form = document.getElementById("registration-form");
-
-
-
-
-function validateInput(firstName, lastName, email, username, password, secondPassword){
-    var nError = document.getElementById("name-error");
-    var eError = document.getElementById("email-error");
-    var unError = document.getElementById("username-error");
-    var pError = document.getElementById("password-error");
-    var rpError = document.getElementById("retype-password-error");
-    var letters = /^[A-Za-z]+$/;
-    
-    if(!firstName.match(letters) || !lastName.match(letters)){
-        nError.innerHTML = "<small style=\"color:Red\">First and last name may only contain letters<small>";
-        return false;
-    }
-    
-    if(lastName==="" || lastName==null){
-        // messages.push("Last name is required")
-    }
-    
-    if(email==="" || email==null){
-        // messages.push("Email is required")
-    }
-    
-    if(username==="" || username==null){
-        // messages.push("Username is required")
-    }
-    
-    if(password==="" || password==null){
-        // messages.push("Password is required")
-    }
-}
-
-form.addEventListener("submit", submitListener);
-
-function submitListener(register){
+document.getElementById("registration-form").addEventListener("submit", function(register){
     register.preventDefault(); 
 
     var firstName = document.getElementById("first-name").value;
@@ -46,31 +9,73 @@ function submitListener(register){
     var secondPassword = document.getElementById("repeat-password").value;
 
     var isValidInput = validateInput(firstName, lastName, email, username, password, secondPassword);
-    // while(!isValidInput){
-    //     isValidInput = validateInput(firstName, lastName, email, username, password, secondPassword);
-    // }
-    if (isValidInput){
-        submit();
-    } else {
-        form.addEventListener("submit", submitListener);
+
+    if(isValidInput) {
+        var credentials = {
+            firstName:document.getElementById("first-name").value,
+            lastName:document.getElementById("last-name").value,
+            email:document.getElementById("email").value,
+            username:document.getElementById("username").value,
+            password:document.getElementById("password").value
+            };
+            fetch("http://localhost:8082/register", {
+                method: 'post',
+                headers: new Headers({
+                    'Content-Type':'application/json'
+                }),
+                body: JSON.stringify(credentials)
+            }).then(response => response.json())
+                .then(data => console.log(data))
+                .catch(error => console.error(error));
+    } 
+});
+
+function validateInput(firstName, lastName, email, username, password, secondPassword){
+    var nameError = document.getElementById("name-error");
+    nameError.hidden = true;
+    var emailError = document.getElementById("email-error");
+    emailError.hidden = true;
+    var usernameError = document.getElementById("username-error");
+    usernameError.hidden = true;
+    var passwordError = document.getElementById("password-error");
+    passwordError.hidden = true;
+    var retypePasswordError = document.getElementById("retype-password-error");
+    retypePasswordError.hidden = true;
+
+    var letters = /^[A-Za-z]+$/;
+    if(!firstName.match(letters) || !lastName.match(letters)){
+        nameError.hidden = false;
+        return false;
     }
+    
+    // just looks for string of the form string@string.string proper validation should be done with a validation link sent to the email address
+    var emailCheck = /\S+@\S+\.\S+/; 
+    if(!emailCheck.test(email)){
+        emailError.hidden = false;
+        return false;
+    }
+    
+    if(!usernameCheck(username)){
+        usernameError.hidden = false;
+        return false;
+    }
+    
+    var passCheck = /[\s~`!@#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?()\._]/g;
+    if(!passCheck.test(password) || password.length<8){
+        passwordError.hidden = false;
+        return false;
+    }
+
+    if(password!=secondPassword){
+        retypePasswordError.hidden = false;
+        return false;
+    }
+    return true;
 }
 
-function submit(){
-    const credentials = {
-        firstName:document.getElementById("first-name").value,
-        lastName:document.getElementById("last-name").value,
-        email:document.getElementById("email").value,
-        username:document.getElementById("username").value,
-        password:document.getElementById("password").value
-        };
-        fetch("http://localhost:8082/register", {
-            method: 'post',
-            headers: new Headers({
-                'Content-Type':'application/json'
-            }),
-            body: JSON.stringify(credentials)
-        }).then(response => response.json())
-            .then(data => console.log(data))
-            .catch(error => console.error(error)); 
+function usernameCheck(username){
+    fetch(`http://localhost:8082/register/${username}`, {
+        mode: 'no-cors'
+        }).then(response => response.json)
+        .then(data => console.log(data.username))
 }
