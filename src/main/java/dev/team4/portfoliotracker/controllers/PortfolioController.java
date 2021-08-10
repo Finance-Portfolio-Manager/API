4f3395a321a66a1bc7e39a876953d7fb7709e067
NPBilotta <nick@bilotta.org>
2021-08-10 16:14:53 -0400

    small change

--- src/main/java/dev/team4/portfoliotracker/controllers/PortfolioController.java
+++ src/main/java/dev/team4/portfoliotracker/controllers/PortfolioController.java
@@ -27,6 +27,10 @@ public class PortfolioController {
     PortfolioService portfolioService;
 
     @Autowired
+    FavoriteService favoriteService;
+    //This will be dependant on merging other branches
+
+    @Autowired
     TransactionServiceImpl transactionService;
 
     @GetMapping("/{id}")
@@ -55,6 +59,7 @@ public class PortfolioController {
                 if (symbols.contains(transaction.getStockSymbol())) {
                     //If stock has already been added, skip
                     continue;
+
                 } else {
                     //add to list of strings tracking symbols
                     symbols.add(transaction.getStockSymbol());
@@ -63,7 +68,9 @@ public class PortfolioController {
                     Stock tempStock = new Stock(transaction);
                     //Add new object to list of Stocks
                     portStocks.add(tempStock);
+
                 }
+
             }
 
             //After making the list of stocks and initializing their first quantity, update all quantities
@@ -86,5 +93,67 @@ public class PortfolioController {
         return new ResponseEntity<>(responsePorts, HttpStatus.OK);
     }
 
+    @GetMapping("/{id}/favorites")
+    public ResponseEntity<List<PortfolioFrontEnd>> getFavoritesByUserId(@PathVariable("id")int id) {
+        //Similar process to creating the user's portfolios, but using the favorites table to grab the IDs
+
+        //List of portfolios mapped to database returned by the service method
+        List<Portfolio> favoritePortfolios = favoriteService.getFavoritesByUserId(id);
+
+        //Prepped list of full portfolio objects to be sent back as the response entity
+        List<PortfolioFrontEnd> responsePorts = new ArrayList<>();
+
+        //List of stocks to be assigned to the portfolio
+        List<Stock> portStocks = new ArrayList<>();
+        //Temp list of stock symbols for tracking in the loop
+        List<String> symbols = new ArrayList<>();
+
+        for (Portfolio portfolio : favoritePortfolios) {
+            //Create new response portfolio with portfolio table as constructor parameter
+            PortfolioFrontEnd portObj = new PortfolioFrontEnd(portfolio);
+            //Will rely on creating List of Stock objects with transaction methods
+            portObj.setTransactions(transactionService.getAllTransactionsByUserId(id));
+
+            //Create the list of Stock objects
+            for (Transaction transaction : portObj.getTransactions()) {
+                //At each transaction, check if a stock symbol has been recorded
+
+                if (symbols.contains(transaction.getStockSymbol())) {
+                    //If stock has already been added, skip
+                    continue;
+
+                } else {
+                    //add to list of strings tracking symbols
+                    symbols.add(transaction.getStockSymbol());
+                    //Create new stock object for this portfolio by passing the first transaction for it as arg
+                    //Initialized quantity is the quantity of the first transaction
+                    Stock tempStock = new Stock(transaction);
+                    //Add new object to list of Stocks
+                    portStocks.add(tempStock);
+
+                }
+
+            }
+
+            //After making the list of stocks and initializing their first quantity, update all quantities
+            for (Stock stock : portStocks) {
+                //The method named here has not been created at the time of writing, but its function is planned
+                //will return list of transactions from a portfolio that matches one stock symbol
+                List<Transaction> relevantTransactions = portfolioService.getTransactionByPortfolioIdAndStockSymbol();
+                stock.setQuantity(relevantTransactions);
+            }
+
+            //Compile remaining details about the portfolio before adding to response list
+            portObj.setStocks(portStocks);
+            portObj.setValue(portStocks);
+
+            responsePorts.add(portObj);
+
+        }
+
+
+        return new ResponseEntity<>(responsePorts, HttpStatus.OK);
+
+    }
 
 }
