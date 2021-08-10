539e8faf66ac1ff3af8f6d52bd2b07968b0ddd49
Marc E Hartley <88401038+har09028@users.noreply.github.com>
2021-08-10 07:48:23 -0700

    added mapping to favorites

--- src/main/java/dev/team4/portfoliotracker/controllers/FavoritesController.java
+++ src/main/java/dev/team4/portfoliotracker/controllers/FavoritesController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/accounts/{user_id}/favorites")
 public class FavoritesController {
 
     @Autowired
