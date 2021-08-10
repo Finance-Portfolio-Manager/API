26ce4828ecdc901ff0f85d2252487ec82133850c
apurvp8 <apurv.patel@revature.net>
2021-08-10 16:07:59 -0400

    Transaction-path

--- src/main/java/dev/team4/portfoliotracker/controllers/TransactionController.java
+++ src/main/java/dev/team4/portfoliotracker/controllers/TransactionController.java
@@ -1,7 +1,6 @@
 package dev.team4.portfoliotracker.controllers;
 
 import dev.team4.portfoliotracker.models.Transaction;
-import dev.team4.portfoliotracker.models.User;
 import dev.team4.portfoliotracker.security.JwtUtility;
 import dev.team4.portfoliotracker.services.TransactionService;
 import dev.team4.portfoliotracker.services.UserDetailsService;
@@ -11,7 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
--- src/main/java/dev/team4/portfoliotracker/services/StockServiceImpl.java
+++ src/main/java/dev/team4/portfoliotracker/services/StockServiceImpl.java
@@ -1,53 +1,53 @@
-package dev.team4.portfoliotracker.services;
-
-import java.util.List;
-
-import javax.transaction.Transactional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import dev.team4.portfoliotracker.models.Stock;
-import dev.team4.portfoliotracker.repositories.StockRepository;
-
-@Service
-@Transactional
-public class StockServiceImpl implements StockService{
-	
-	@Autowired
-	StockRepository stockRepo;
-
-	@Override
-	public Stock addStock(Stock stock) {
-		return stockRepo.save(stock);
-	}
-
-	@Override
-	public Stock addStock(int userId, String stockSymbol, double stockQuantity) {
-		Stock s = new Stock(userId, stockSymbol, stockQuantity);
-		return stockRepo.save(s);
-	}
-
-	@Override
-	public void deleteStock(int stockId) {
-		Stock s = stockRepo.findStockByStockId(stockId);
-		stockRepo.delete(s);
-	}
-
-	@Override
-	public Stock updateStockQuantity(int stockId, double stockQuantity) {
-		Stock s = stockRepo.findStockByStockId(stockId);
-		s.setStockQuantity(stockQuantity);
-		return stockRepo.save(s);
-	}
-
-	@Override
-	public Stock getStock(int stockId) {
-		return stockRepo.findStockByStockId(stockId);
-	}
-
-	@Override
-	public List<Stock> getAllStocks(int userId) {
-		return stockRepo.findAllStocksByUserId(userId);
-	}
-}
+//package dev.team4.portfoliotracker.services;
+//
+//import java.util.List;
+//
+//import javax.transaction.Transactional;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import dev.team4.portfoliotracker.models.Stock;
+//import dev.team4.portfoliotracker.repositories.StockRepository;
+//
+//@Service
+//@Transactional
+//public class StockServiceImpl implements StockService{
+//
+//	@Autowired
+//	StockRepository stockRepo;
+//
+//	@Override
+//	public Stock addStock(Stock stock) {
+//		return stockRepo.save(stock);
+//	}
+//
+//	@Override
+//	public Stock addStock(int userId, String stockSymbol, double stockQuantity) {
+//		Stock s = new Stock(userId, stockSymbol, stockQuantity);
+//		return stockRepo.save(s);
+//	}
+//
+//	@Override
+//	public void deleteStock(int stockId) {
+//		Stock s = stockRepo.findStockByStockId(stockId);
+//		stockRepo.delete(s);
+//	}
+//
+//	@Override
+//	public Stock updateStockQuantity(int stockId, double stockQuantity) {
+//		Stock s = stockRepo.findStockByStockId(stockId);
+//		s.setStockQuantity(stockQuantity);
+//		return stockRepo.save(s);
+//	}
+//
+//	@Override
+//	public Stock getStock(int stockId) {
+//		return stockRepo.findStockByStockId(stockId);
+//	}
+//
+//	@Override
+//	public List<Stock> getAllStocks(int userId) {
+//		return stockRepo.findAllStocksByUserId(userId);
+//	}
+//}
