7995ffd4055f911f0bdf3fa5e425ac2bc6c1fc85
Dgarc359 <87497958+Dgarc359@users.noreply.github.com>
2021-08-11 19:21:54 -0400

    adding post mapping /post-news

--- src/main/java/dev/team4/portfoliotracker/controllers/NewsController.java
+++ src/main/java/dev/team4/portfoliotracker/controllers/NewsController.java
@@ -2,7 +2,9 @@ package dev.team4.portfoliotracker.controllers;
 
 import dev.team4.portfoliotracker.models.News;
 import dev.team4.portfoliotracker.services.NewsService;
+import io.swagger.models.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +19,15 @@ public class NewsController {
     @GetMapping(value="/get-news",produces = "application/json")
     public ResponseEntity<News> getNews(){
         return ResponseEntity.ok().body(newsService.findNewsById(1));
+    }
+
+    @PostMapping(value="/post-news",consumes="application/json", produces="application/json")
+    public ResponseEntity<News> saveNews(
+            @RequestBody String title, String description, String url, String urlToImage, int newsId){
+        News news = new News(newsId, title, description, url, urlToImage);
+
+
+        return new ResponseEntity<News>(newsService.saveNews(news), HttpStatus.OK);
     }
 
 //    @PostMapping(value="/post-news",consumes="application/json", produces="application/json")
