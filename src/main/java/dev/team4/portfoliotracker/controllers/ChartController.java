76c3a761ae7be002b3961ea9468c22fa8ac649ae
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-08-11 17:28:56 -0700

    not working

---
+++ src/main/java/dev/team4/portfoliotracker/controllers/ChartController.java
@@ -0,0 +1,70 @@
+package dev.team4.portfoliotracker.controllers;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/charts")
+public class ChartController {
+
+    @Autowired
+    Environment environment;
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @GetMapping("")
+    public ResponseEntity please(){
+        logger.info("please");
+        logger.info("please");
+        logger.info("please");
+        logger.info("please");
+        logger.info("please");
+        logger.info("please");
+        logger.info("please");
+        logger.info("please");
+        logger.info("please");
+        logger.info("please");
+        logger.info("please");
+        return new ResponseEntity<>("please", HttpStatus.OK);
+    }
+
+ 	// @GetMapping("/{symbol}")
+	// public ResponseEntity<String> getChart(
+    //     @PathVariable("symbol") String symbol
+    // ) {
+    //     logger.info(symbol);
+    //     logger.info(symbol);
+    //     logger.info(symbol);
+    //     logger.info(symbol);
+    //     logger.info(symbol);
+    //     logger.info(symbol);
+    //     logger.info(symbol);
+    //     logger.info(symbol);
+    //     logger.info(symbol);
+    //     logger.info(symbol);
+    //     logger.info(symbol);
+	// 	HttpResponse<String> response = null;
+	// 	String uri = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MCD&apikey="+environment.getProperty("KEYS_ALPHAVANTAGE");
+	// 	try {
+	// 		response = Unirest.get(uri).asString();
+	// 	} catch (UnirestException e) {
+	// 		e.printStackTrace();
+	// 	}
+	// 	return new ResponseEntity<String>(response.getBody(), HttpStatus.OK);
+	// }
+
+}
